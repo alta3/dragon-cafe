71bdbd0c713b8d32fb39612c0542a6a7bdf697c9
seaneon <sean.r.barbour@gmail.com>
2022-06-06 20:49:11 -0400

    menu.py

--- microservices/menu/menu.py
+++ microservices/menu/menu.py
@@ -1,23 +1,32 @@
-from pathlib import Path
-import os
-import jinja2
-import socket
-import requests
+#!/usr/bin/env python3
+
+"""
+Dragon Cafe Menu Microservice | Author: Sam Griffith | Org: Alta3 Research Inc.
+
+This Menu Microservice is the first part of the monolithic application of a Chinese Restaurant website that has been broken out into it's own service.
+"""
+
 from aiohttp import web
+import jinja2
+from pathlib import Path
+import random
+import requests
+import socket
+import os
 
 HOST = os.getenv("MENU_HOST", "0.0.0.0")
 LOCAL_IP = socket.gethostbyname(socket.gethostname())
 PORT = os.getenv("MENU_PORT", 2227)
 REG_ADDR = os.getenv("SR_ADDRESS", "127.0.0.1")
-#REG_ADDR = "10.17.133.17"
 REG_PORT = os.getenv("SR_PORT", 55555)
 SERVICE = os.path.basename(__file__).rstrip(".py")
+
 
 class Page:
     def __init__(self, filename, templates_dir=Path("templates"), args={}, cookies={}):
         """
         Create a new instance of an html page to be returned
-        :param fillename: name of file found in the templates_dir
+        :param filename: name of file found in the templates_dir
         """
         self.path = templates_dir
         self.file = templates_dir / filename
@@ -34,6 +43,7 @@ class Page:
                 resp.set_cookie(c, j)
             return resp
 
+
 def routes(app: web.Application) -> None:
     app.add_routes(
         [
@@ -45,8 +55,9 @@ def routes(app: web.Application) -> None:
 
 async def menu(request) -> web.Response:
     """
-    This will return the menu
+    This will return the jinja2 templated menu.html file.
     """
+    print(request)
     food_items = [
         {"item": "General Tzo's Chicken", "description": "Yummy chicken on rice", "price": 12.99},
         {"item": "Kung Pao Beef", "description": "Spicy Beef on rice", "price": 13.99}
@@ -56,28 +67,43 @@ async def menu(request) -> web.Response:
     return page.render()
 
 
-def register(service):
-    r = requests.get(f"http://{REG_ADDR}:{REG_PORT}/add/{service}/{LOCAL_IP}/{PORT}")
-    print(r.status_code)
+async def register(add_to_registry=True):
+    print(f"""
+    Service Registry {REG_ADDR}:{REG_PORT}
+
+    Adding to the Service Registry:
+
+    service name     {SERVICE}
+    service IP       {LOCAL_IP}
+    service port     {PORT}
+    """)
+    if add_to_registry:
+        r = requests.get(f"http://{REG_ADDR}:{REG_PORT}/add/{SERVICE}/{LOCAL_IP}/{PORT}")
+        print(r.status_code, r.text)
 
 
-def unregister(service):
-    r = requests.get(f"http://{REG_ADDR}:{REG_PORT}/remove/{service}/{LOCAL_IP}/{PORT}")
-    print(r.status_code)
+async def unregister(remove_from_registry=True):
+    if remove_from_registry:
+        r = requests.get(f"http://{REG_ADDR}:{REG_PORT}/remove/{SERVICE}/{LOCAL_IP}/{PORT}")
+        print(r.status_code)
 
 
 def main():
-    print("The menu microservice web server is starting up!")
+    """
+    This is the main process for the aiohttp server.
+
+    This works by instantiating the app as a web.Application(),
+    then applying the setup function we built in our routes
+    function to add routes to our app, then by starting the async
+    event loop with web.run_app().
+    """
+
+    print("This aiohttp web server is starting up!")
     app = web.Application()
     routes(app)
-    try:
-        register(SERVICE)
-    except requests.exceptions.ConnectionError as err:
-        print(err)
-    try:
-        web.run_app(app, host=HOST, port=PORT)
-    except KeyboardInterrupt:
-        unregister(SERVICE)
+    app.on_startup.append(register)
+    app.on_shutdown.append(unregister)
+    web.run_app(app, host=HOST, port=PORT)
 
 
 if __name__ == "__main__":
